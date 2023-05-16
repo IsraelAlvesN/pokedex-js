9bb94c5de7a03917fbe8e071a2a67c542657b26f
IsraelAlvesN <nicolauprog21@gmail.com>
2023-05-16 14:59:23 -0300

    Requesting in API and showing the responses in the HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,38 @@
-const btnTest = document.getElementById('btnTest')
-btnTest.addEventListener('click', function(){
-    btnTest.style.backgroundColor = 'red'
-})
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
+function convertPokemonToLi(pokemon){
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonOl = document.getElementById('pokemonList')
+
+fetch(url)
+    .then((res) => res.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+        for (let i = 0; i < pokemonList.length; i++) {
+            const pokemon = pokemonList[i];
+
+            pokemonOl.innerHTML += convertPokemonToLi(pokemon)
+
+        }
+    })
+    .catch((error) => console.error(error))
+    .finally(function (){
+        //responde independente do que aconteceu
+        console.log("Requisição concluída!!!")
+    })
